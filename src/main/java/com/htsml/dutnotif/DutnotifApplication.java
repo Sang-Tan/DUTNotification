30d89d5fa1a0c5b2bab49d0f2df295f97ad344f1
SPRV <sangprovai14@gmail.com>
2024-01-23 07:48:50 +0700

    first commit

---
+++ src/main/java/com/htsml/dutnotif/DutnotifApplication.java
@@ -0,0 +1,13 @@
+package com.htsml.dutnotif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutnotifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DutnotifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/htsml/dutnotif/DutnotifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htsml.dutnotif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutnotifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
