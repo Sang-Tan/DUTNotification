3c2c58d75ae40728a5efd4eacb22bf3f38b72541
SPRV <sangprovai14@gmail.com>
2024-03-01 15:47:22 +0700

    hotfix: spring boot build

--- src/test/java/com/htsml/dutnotif/DutnotifApplicationTests.java
+++ src/test/java/com/htsml/dutnotif/DutnotifApplicationTests.java
@@ -3,7 +3,7 @@ package com.htsml.dutnotif;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DutnotifApplication.class)
 class DutnotifApplicationTests {
 
     @Test
